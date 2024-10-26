af94e4a25f6ef9cd74ae8461d49d141b986ca3a7
Arttu Puttonen <arskaputtonen@gmail.com>
2024-10-27 00:29:28 +0300

    joo

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -11,7 +11,7 @@ const Skills = () => {
                 <p className="text-sm text-designColor tracking-[4px]">Features</p>
                 <h2 className="text-4xl font-bold">Design Skills</h2>
             </div>
-            <div className="w-full mt-14 w-full flex flex-col gap-6">
+            <div className="w-full mt-14 flex flex-col gap-6">
                 <div className="overflow-x-hidden">
                     <p className="text-sm uppercase font-medium">Photography</p>
                     <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
@@ -35,17 +35,112 @@ const Skills = () => {
                         </motion.span>
                     </span>
                 </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Photography</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}}
+                        className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">100%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Editing</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}} className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">90%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Editing</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}} className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">90%</span>
+                        </motion.span>
+                    </span>
+                </div>
+            
             </div>
             </div>
+
+
 
 
 
             <div className="w-1/2">
             <div className="mt-16 py-6 font-titleFont">
                 <p className="text-sm text-designColor tracking-[4px]">Features</p>
-                <h2 className="text-4xl font-bold">Development Skills</h2>
+                <h2 className="text-4xl font-bold">Design Skills</h2>
             </div>
-            <div className="w-full mt-14">
+            <div className="w-full mt-14 flex flex-col gap-6">
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Photography</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}}
+                        className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">100%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Editing</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}} className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">90%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Photography</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}}
+                        className="w-full h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">100%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Editing</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}} className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">90%</span>
+                        </motion.span>
+                    </span>
+                </div>
+                <div className="overflow-x-hidden">
+                    <p className="text-sm uppercase font-medium">Editing</p>
+                    <span className="w-full h-2 bgOpacity inline-flex rounded-md mt-2">
+                        <motion.span
+                        initial={{x:"-100%", opacity:0}}
+                        animate={{x:0, opacity:1,}}
+                        transition={{duration: 0.5, delay:0.5}} className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative">
+                            <span className="absolute -top-7 right-0">90%</span>
+                        </motion.span>
+                    </span>
+                </div>
+            
             </div>
             </div>
             </motion.div>
