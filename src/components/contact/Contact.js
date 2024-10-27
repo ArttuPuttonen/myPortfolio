c062d2159a0749a21ed632d1bdebcdaed2f59a70
Arttu Puttonen <arskaputtonen@gmail.com>
2024-10-27 21:27:06 +0200

    välipush

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Title from '../layouts/Title';
-import {hand_shake} from '../../assets/index';
-import { FaFacebookF, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import ContactLeft from './ContactLeft';
+
 
 const Contact = () => {
+    const [username, setUsername] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
+
+
+    const handleSend = (e) => {
+        e.preventDefault()
+        console.log('Message sent')
+    }
+
     return(
         <section id='contact' className="w-full py-20 border-b-[1px] border-b-black">
         <div className="flex justify-center items-center text-center">
@@ -12,49 +26,46 @@ const Contact = () => {
         </div>
         <div className="w-full">
             <div className="w-full h-auto flex justify-between">
-                <div className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
-                    <img
-                     className="w-full h-46 object-cover rounded-lg mb-2"
-                     src={hand_shake} alt="contactImg"/>
-                     <div className="flex flex-col gap-4">
-                        <h3 className="text-3xl font-bold text-white">Arttu Puttonen</h3>
-                        <p className="text-lg font-normal text-gray-400">
-                            Student and Freelancer
-                        </p>
-                        <p className="text-base text-gray-400 tracking-wide">
-                            I am a student and freelancer from Finland. I am currently studying in the field of IT and I am looking for new opportunities to work in the field of IT.
-                        </p>
-                        <p className="text-base text-gray-400 flex items-center gap-2">
-                            Phone: <span className="text-lightText">+358 45 8445 940</span>
-                        </p>
-                        <p className="text-base text-gray-400 flex items-center gap-2">
-                            Email: <span className="text-lightText">arskaputtonen@gmail.com</span>
-                        </p>
-                     </div>
-                     <div className="flex flex-col">
-                        <h2 className="text-base uppercase font-titleFont mb-4">
-                            Find me on
-                        </h2>
-                        <div className="flex gap-4">
-                <span className="bannerIcon">
-                    <FaFacebookF />
-                </span>
-                <span className="bannerIcon">
-                    <FaInstagram />
-                </span>
-                <span className="bannerIcon">
-                    <FaLinkedin />
-                </span>
-                <span className="bannerIcon">
-                    <FaGithub />
-                </span>
-            </div>
-                     </div>
-                </div>
+                <ContactLeft />
 
 
-                <div className="w-[60%] h-full flex flex-col justify-between">
-
+                <div className="w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#2327b]
+                flex flex-col gap-8 p-8 rounded-lg shadow-shadowOne">
+                    <form className="w-full flex flex-col gap-6 py-2">
+                        <div className="w-full flex gap-10">
+                            <div className="w-1/2 flex flex-col gap-4">
+                            <p className="text-sm text-gray-400 uppercase tracking-wide">YOUR NAME</p>
+                            <input className="contactInput" 
+                            type="text"/>
+                            </div>
+                            <div className="w-1/2 flex flex-col gap-4">
+                            <p className="text-sm text-gray-400 uppercase tracking-wide">Phone number</p>
+                            <input className="contactInput" 
+                            type="text"/>
+                            </div>
+                        </div>
+                        <div className="w-full flex flex-col gap-4">
+                            <p className="text-sm text-gray-400 uppercase tracking-wide">Email</p>
+                            <input className="contactInput" 
+                            type="email"/>
+                        </div>
+                        <div className="w-full flex flex-col gap-4">
+                            <p className="text-sm text-gray-400 uppercase tracking-wide">Subject</p>
+                            <input className="contactInput" 
+                            type="text"/>
+                        </div>
+                        <div className="w-full flex flex-col gap-4">
+                            <p className="text-sm text-gray-400 uppercase tracking-wide">Message</p>
+                            <textarea className="contactTextArea" cols="30" rows="8"></textarea>
+                        </div>
+                        <div w-full>
+                            <button onClick={handleSend} className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400
+                            tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor
+                            border-transparent">
+                                Send Message
+                                </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
---
+++ src/components/contact/ContactLeft.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { hand_shake } from '../../assets/index';
+
+const ContactLeft = () => {
+    return(
+        <div className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+        <img
+         className="w-full h-46 object-cover rounded-lg mb-2"
+         src={hand_shake} alt="contactImg"/>
+         <div className="flex flex-col gap-4">
+            <h3 className="text-3xl font-bold text-white">Arttu Puttonen</h3>
+            <p className="text-lg font-normal text-gray-400">
+                Student and Freelancer
+            </p>
+            <p className="text-base text-gray-400 tracking-wide">
+                I am a student and freelancer from Finland. I am currently studying in the field of IT and I am looking for new opportunities to work in the field of IT.
+            </p>
+            <p className="text-base text-gray-400 flex items-center gap-2">
+                Phone: <span className="text-lightText">+358 45 8445 940</span>
+            </p>
+            <p className="text-base text-gray-400 flex items-center gap-2">
+                Email: <span className="text-lightText">arskaputtonen@gmail.com</span>
+            </p>
+         </div>
+         <div className="flex flex-col">
+            <h2 className="text-base uppercase font-titleFont mb-4">
+                Find me on
+            </h2>
+            <div className="flex gap-4">
+    <span className="bannerIcon">
+        <FaFacebookF />
+    </span>
+    <span className="bannerIcon">
+        <FaInstagram />
+    </span>
+    <span className="bannerIcon">
+        <FaLinkedin />
+    </span>
+    <span className="bannerIcon">
+        <FaGithub />
+    </span>
+</div>
+         </div>
+    </div>
+    )
+}
+
+
+export default ContactLeft;
