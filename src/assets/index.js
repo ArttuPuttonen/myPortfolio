57f50393cdbbd4047fb55a2b0e2cf28bdcd931a0
Arttu Puttonen <arskaputtonen@gmail.com>
2024-10-27 20:16:43 +0200

    contactia

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,12 @@
 import logo from './images/logo-example-removebg-preview.png';
 import bannerImg from './images/bannerImg.png';
 import kuu from './images/kuu.jpg';
+import hand_shake from './images/hand_shake.jpeg';
 
 
 export {
     logo,
     bannerImg,
-    kuu
+    kuu,
+    hand_shake
 }
