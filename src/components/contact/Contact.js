65d15a3dab271bc6e07af80e911f64f452ab3336
Arttu Puttonen <arskaputtonen@gmail.com>
2024-10-27 21:38:29 +0200

    välipushia

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -13,11 +13,40 @@ const Contact = () => {
     const [successMsg, setSuccessMsg] = useState("");
 
 
-    const handleSend = (e) => {
-        e.preventDefault()
-        console.log('Message sent')
-    }
+  // ========== Email Validation start here ==============
+  const emailValidation = () => {
+    return String(email)
+      .toLocaleLowerCase()
+      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  };
+  // ========== Email Validation end here ================
 
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (username === "") {
+      setErrMsg("Name is required!");
+    } else if (phoneNumber === "") {
+      setErrMsg("Phone number is required!");
+    } else if (email === "") {
+      setErrMsg("Please give your Email!");
+    } else if (!emailValidation(email)) {
+      setErrMsg("Give a valid Email!");
+    } else if (subject === "") {
+      setErrMsg("Plese give your Subject!");
+    } else if (message === "") {
+      setErrMsg("Message is required!");
+    } else {
+      setSuccessMsg(
+        `Thank you dear ${username}, Your Messages has been sent Successfully!`
+      );
+      setErrMsg("");
+      setUsername("");
+      setPhoneNumber("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    }
+  };
     return(
         <section id='contact' className="w-full py-20 border-b-[1px] border-b-black">
         <div className="flex justify-center items-center text-center">
@@ -35,7 +64,7 @@ const Contact = () => {
                         <div className="w-full flex gap-10">
                             <div className="w-1/2 flex flex-col gap-4">
                             <p className="text-sm text-gray-400 uppercase tracking-wide">YOUR NAME</p>
-                            <input className="contactInput" 
+                            <input onChange={(e)=>setUsername(e.target.value)}  className="contactInput" 
                             type="text"/>
                             </div>
                             <div className="w-1/2 flex flex-col gap-4">
